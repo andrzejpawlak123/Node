3d0c540508850f7dec83ea4f2665d1d52c1bd78b
Andrzej Pawlak <andrzej_pawlak90@o2.pl>
2018-06-27 16:32:59 +0200

    create database

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const mysql = require('mysql');
 
+const db = mysql.createConnection({
+host : 'localhost',
+user:  'root',
+password: 'pass'
+});
+
+db.connect((err)=>{
+
+    console.log('mysql connect..');
+
+
+db.query("CREATE DATABASE dbexample", function (err, result) {
+    if (err)throw err;
+    console.log("Database created");
+    console.log(result);
+    
+  });
+});
+
+
 const app = express();
+
+
 
 app.listen('8080',()=>{
 console.log('Server listen on port 8080');
 
 })
+
+
